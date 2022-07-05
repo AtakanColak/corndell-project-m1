d8215a864ddb162f1b3c5f469ffa60a816da04ba
Atakan Colak <atakan.colak@softwire.com>
2022-07-05 14:25:05 +0100

    Fix gunicorn

--- todo_app/wsgi.py
+++ todo_app/wsgi.py
@@ -1,5 +1,5 @@
 from dotenv import find_dotenv, load_dotenv
-from todo_app import create_app
+from todo_app.app import create_app
 from todo_app.data.trello_service import TrelloService
 
 app = create_app()
